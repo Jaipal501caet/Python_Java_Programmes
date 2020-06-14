897e5d7816d1f8acb9adfdb8d80bd70a145b0fc8
Jaipal <chaudhary.jaipal@gmail.com>
2020-06-14 20:10:10 +0530

    compreshsive scenario

---
+++ PycharmProjects/Project1/list_comprehension.py
@@ -0,0 +1,15 @@
+num = [5,4,7,9,12,15,22,75,100,107]
+
+list_div2 = [i for i in num if i % 2 ==0]
+print(list_div2)
+
+list_div5 = [i for i in num if i % 5 ==0]
+print(list_div5)
+
+list_div2or3 = [i for i in num if i%2==0 or i%3==0]
+print(list_div2or3)
+
+
+
+
+
